197b677f7c1a5ddb08533fbcc2f58492d1dcaf40
Briseus <ville.v.aaltonen@gmail.com>
2016-02-13 23:13:45 +0200

    siirretty angular moduulit app.js tiedostoon

--- js/app.js
+++ js/app.js
@@ -1,3 +1,155 @@
+
+  // luodaan module nimellä scotchApp
+  var scotchApp = angular.module('scotchApp', ['ngRoute', 'ngCookies']);
+
+  // määritetään mistä sivun sisältö löytyy
+  scotchApp.config(function($routeProvider) {
+
+    $routeProvider
+      .when('/', {
+      templateUrl: 'views/etusivu.html',
+      controller: 'mainController'
+    })
+
+    .when('/video', {
+      templateUrl: 'views/video.html'
+    })
+
+    .when('/credits', {
+      templateUrl: 'views/credits.html',
+    })
+
+    .when('/kello', {
+      templateUrl: 'views/kello.html',
+      controller: 'kelloController'
+    })
+
+    .when('/kello', {
+      templateUrl: 'views/kello.html',
+      controller: 'kelloController'
+    })
+
+    .when('/kello/aika/:time/nimi/:name', {
+      templateUrl: 'views/kello.html',
+      controller: 'kelloTimeAndNameUrlController'
+    })
+
+    .when('/kello/aika/:time', {
+      templateUrl: 'views/kello.html',
+      controller: 'kelloTimeUrlController'
+    });
+
+  });
+
+
+  // luodaan ohjain etusivulle
+  scotchApp.controller('mainController', function($scope, $cookies, $location) {
+
+    $scope.generateKuumotus = function() {
+
+      var name = document.getElementById('kuumotus_nimi').value;
+      var inserted_day = document.getElementById('kuumotus_paivamaara').value;
+      var inserted_time = document.getElementById('kuumotus_aika').value;
+      var time_and_day = inserted_day + " " + inserted_time;
+      target_date = new Date(time_and_day);
+      if ($cookies.get('day') !== null) {
+        $cookies.remove('day');
+      }
+      if ($cookies.get('name') !== null) {
+        $cookies.remove('name');
+      }
+      $cookies.put('name', name);
+      $cookies.put('day', target_date.getTime());
+
+      $location.path();
+      $location.path('/kello');
+      window.location = '#/kello';
+      window.location.reload();
+
+    };
+
+  });
+
+  scotchApp.controller('kelloController', function($scope, $cookies) {
+    var target_date = $cookies.get('day');
+    if ($cookies.get('name') === undefined) {
+      $scope.name = "Meneppäs generoimaan kuumotus";
+    } else {
+      $scope.name = $cookies.get('name') + "  armon aikaa jäljellä";
+    }
+
+    startClock(target_date);
+
+      $scope.destroyKuumotus = function() {
+        $cookies.remove('day');
+        $cookies.remove('name');
+        window.location.reload();
+      };
+  });
+
+  scotchApp.controller('kelloTimeAndNameUrlController', function($scope, $cookies,$routeParams) {
+    if ($cookies.get('day') !== null) {
+      $cookies.remove('day');
+    }
+    if ($cookies.get('name') !== null) {
+      $cookies.remove('name');
+    }
+
+    var target_date = $routeParams.time;
+    if ($routeParams.name === null) {
+      $scope.name = "";
+    } else {
+      $scope.name = $routeParams.name + "  armon aikaa jäljellä";
+    }
+    var name = $routeParams.name;
+    $cookies.put('name', name);
+    $cookies.put('day', target_date);
+    startClock(target_date);
+    window.location = '#/kello';
+    window.location.reload();
+  });
+
+  scotchApp.controller('kelloTimeUrlController', function($scope, $cookies,$routeParams,$location) {
+    if ($cookies.get('day') !== null) {
+      $cookies.remove('day');
+    }
+    if ($cookies.get('name') !== null) {
+      $cookies.remove('name');
+    }
+
+    var target_date = $routeParams.time;
+    if ($routeParams.name === null) {
+      $scope.name = "";
+    } else {
+      $scope.name = $routeParams.name + "  armon aikaa jäljellä";
+    }
+    var name = "";
+    $cookies.put('name', name);
+    $cookies.put('day', target_date);
+    startClock(target_date);
+    window.location = '#/kello';
+    window.location.reload();
+
+
+    // if ($cookies.get('day') != null) {
+    //   $cookies.remove('day');
+    // }
+    // if ($cookies.get('name') != null) {
+    //   $cookies.remove('name');
+    // }
+    // $scope.name = "";
+    //
+    // $cookies.put('day', target_date);
+    //
+    // $location.path();
+    // $location.path('/kello');
+    // window.location = '#/kello';
+    // window.location.reload();
+  });
+
+
+
+
 function laskuri(target_date) {
     var days, hours, minutes, seconds;
     var target = target_date;
